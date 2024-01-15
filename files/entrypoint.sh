ccf86d7d9ff721298c3829294e0525507c80738a
yizhipian <123932855+yizhipian@users.noreply.github.com>
2024-01-15 11:27:46 +0800

    Update entrypoint.sh

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -2,7 +2,7 @@
 
 # 设置各变量
 WSPATH=${WSPATH:-'argo'}
-UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
+UUID=${UUID:-'de03add9-5c68-8bab-950c-08cd5320df18'}
 WEB_USERNAME=${WEB_USERNAME:-'admin'}
 WEB_PASSWORD=${WEB_PASSWORD:-'password'}
 
